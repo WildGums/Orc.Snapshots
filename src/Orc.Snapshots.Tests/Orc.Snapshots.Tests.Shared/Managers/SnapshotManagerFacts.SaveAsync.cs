0eafc5ecb71b7e577753f0e27f2230175c2baca2
Geert van Horrik <geert@catenalogic.com>
2016-12-07 21:44:07 +0100

    Add unit tests for the SnapshotManager

---
+++ src/Orc.Snapshots.Tests/Orc.Snapshots.Tests.Shared/Managers/SnapshotManagerFacts.SaveAsync.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SnapshotManagerFacts.SaveAsync.cs" company="WildGums">
+//   Copyright (c) 2008 - 2016 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Snapshots.Tests.Managers
+{
+    using NUnit.Framework;
+
+    public partial class SnapshotManagerFacts
+    {
+        [TestFixture]
+        public class TheSaveAsyncMethod
+        {
+
+        }
+    }
+}
