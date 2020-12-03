55c6b0251daad916abf2c6b85c76b8fa90419914
Max <mkhomutov@gmail.com>
2020-12-03 16:12:38 +0400

    fix some issues detected by Sonar

--- src/Orc.Snapshots/Exceptions/InvalidSnapshotException.cs
+++ src/Orc.Snapshots/Exceptions/InvalidSnapshotException.cs
@@ -7,14 +7,28 @@
 
 namespace Orc.Snapshots
 {
+    using System;
+    using System.Runtime.Serialization;
     using Catel;
 
+    [Serializable]
     public class InvalidSnapshotException : SnapshotException
     {
         #region Constructors
+        public InvalidSnapshotException()
+        {
+
+        }
+
         public InvalidSnapshotException(ISnapshot snapshot)
             : base(snapshot, string.Format("Snapshot '{0}' is invalid at this stage", ObjectToStringHelper.ToString(snapshot)))
         {
+        }
+
+        public InvalidSnapshotException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
         }
         #endregion
     }
--- src/Orc.Snapshots/Exceptions/SnapshotManagementInitializationException.cs
+++ src/Orc.Snapshots/Exceptions/SnapshotManagementInitializationException.cs
@@ -8,14 +8,27 @@
 namespace Orc.Snapshots
 {
     using System;
+    using System.Runtime.Serialization;
 
+    [Serializable]
     public class SnapshotManagementInitializationException : Exception
     {
         #region Constructors
+        public SnapshotManagementInitializationException()
+        {
+
+        }
+
         public SnapshotManagementInitializationException(ISnapshotManager snapshotManager)
             : base("Unable to initialize SnapshotManager. Probably initialization was canceled.")
         {
             SnapshotManager = snapshotManager;
+        }
+
+        public SnapshotManagementInitializationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
         }
         #endregion
 
